91c0a9fea2de8b70363253eef584d625786a6c8e
yambal <yambal@gmail.com>
2025-01-13 20:16:07 +0900

    comment

--- src/pages/samples/NotificationPage.tsx
+++ src/pages/samples/NotificationPage.tsx
@@ -1,11 +1,15 @@
 import { Notification } from "../../features/notification/Notification"
-import { Container } from "@chakra-ui/react"
+import { Container, Link } from "@chakra-ui/react"
 
 export const NotificationPage = () => {
   return (
     <>
       <Container><h3>Notification</h3></Container>
       <Container>
+        <p>
+          This sample demonstrates the implementation of "<Link href="https://github.com/tauri-apps/plugins-workspace/tree/v2/plugins/notification">PLUGIN-NOTIFICATION</Link>".<br />
+          The official plugin is available in "<Link href="https://github.com/tauri-apps/plugins-workspace?tab=readme-ov-file">Official Tauri Plugins</Link>".
+        </p>
         <Notification />
       </Container>
     </>
